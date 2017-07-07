cc5309f8292c776728da70d51db764b01ee709a8
KazaFalcon <sameersaxena@dynaptsolutions.com>
2017-07-07 18:21:42 +0530

    Update CronEntry.sh

--- CronEntry.sh
+++ CronEntry.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 camera=$(date +%Y-%m-%d)
-crontab -l > $camera".txt"
+crontab -l > $camera".text"
 #echo new cron into cron file
-        echo "@reboot Date" >> $camera".txt"
-        echo "@reboot Testing &" >> $camera".txt"
+        echo "@reboot Date" >> $camera".text"
+        echo "@reboot Testing &" >> $camera".text"
