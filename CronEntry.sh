951b9291d2cf82f9a31c0ae8f872c23b8413f3a4
KazaFalcon <sameersaxena@dynaptsolutions.com>
2017-07-07 17:36:09 +0530

    Create CronEntry.sh

---
+++ CronEntry.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+camera=$(date +%Y-%m-%d)
+crontab -l > $camera".txt"
+#echo new cron into cron file
+        echo "@reboot Date >> $camera".txt"
+        echo "@reboot Testing &" >> $camera".txt"
