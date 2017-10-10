77d6baeed7e31a845086d09627fe15ff9b798168
KazaFalcon <sameersaxena@dynaptsolutions.com>
2017-10-10 16:21:16 +0530

    Update CronEntry.sh

--- CronEntry.sh
+++ CronEntry.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 camera=$(date +%Y-%m-%d)
-crontab -l > $camera".text"
+crontab -l > $camera".txt"
 #echo new cron into cron file
-        echo "*/1 * * * * sh /home/Aggregator/vehicleStop.sh"
-        echo "@reboot sh /home/Aggregator/drivingTime.sh"
-        echo "@reboot sh /home/Aggregator/driverIdelTime.sh"
+        echo "*/1 * * * * sh /home/Aggregator/vehicleStop.sh" >> $camera".txt"
+        echo "@reboot sh /home/Aggregator/drivingTime.sh" >> $camera".txt"
+        echo "@reboot sh /home/Aggregator/driverIdelTime.sh" >> $camera".txt"
