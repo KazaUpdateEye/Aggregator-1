5328bb6b0cd0f6fb6943722fd7966df2e24520aa
KazaUpdateEye <31059737+KazaUpdateEye@users.noreply.github.com>
2019-03-05 11:53:54 +0530

    Delete CronEntry.sh

--- CronEntry.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-camera=$(date +%Y-%m-%d)
-crontab -l > $camera".text"
-#echo new cron into cron file
-        echo "*/1 * * * * sh /home/Aggregator/vehicleStop.sh" >> $camera".text"
-        echo "@reboot sh /home/Aggregator/drivingTime.sh" >> $camera".text"
-        echo "@reboot sh /home/Aggregator/driverIdelTime.sh" >> $camera".text"
